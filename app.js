ec713584e869b4f9cf54c5b28a6469e168e7d48e
irfanansari1912 <irfanansari1912@outlook.com>
2022-11-16 21:33:32 +0530

    To Do list node js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+var items = []
+app.get("/", function(req, res) {
+    // res.send("this is working fine");
+    res.render("list", { ejes: items });
+
+})
+app.post("/", function(req, res) {
+    var item = req.body.ele1;
+    items.push(item);
+    console.log(items)
+    res.redirect("/")
+
+})
+app.listen(8000, function() {
+    console.log("server started");
+})
